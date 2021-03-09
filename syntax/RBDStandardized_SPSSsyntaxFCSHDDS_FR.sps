6449376f9a380ce2c0c8f01b488614384bdc09ae
olanderb <billyolander@gmail.com>
2021-03-09 14:21:17 +0000

    fixed stuff

--- syntax/RBDStandardized_SPSSsyntaxFCSHDDS_FR.sps
+++ syntax/RBDStandardized_SPSSsyntaxFCSHDDS_FR.sps
@@ -1,5 +1,5 @@
 GET
-  FILE='exampledataFrancais.sav'.
+  FILE='exampledataFrancais_raw.sav'.
 DATASET NAME DataSet1 WINDOW=FRONT.
 
 ** calculer le Score de Consommation Alimentaire
@@ -7,7 +7,7 @@ DATASET NAME DataSet1 WINDOW=FRONT.
 compute FCS = sum(FCSStap*2, FCSPulse*3, FCSDairy*4, FCSPr*4, FCSVeg, FCSFruit, FCSFat*0.5, FCSSugar*0.5).
 variable labels FCS "Score de Consommation Alimentaire".
 
-** calculer  des groupes de consommation alimentaire à partir du score de consommation alimentaire - seuils 21/35 et 28/42s
+** calculer  des groupes de consommation alimentaire Ã  partir du score de consommation alimentaire - seuils 21/35 et 28/42s
 
 recode FCS (0 thru 21 = 1) (21 thru 35 = 2) (35 thru highest = 3) into FCSCat21.
 variable labels FCSCat21 "Groupes de Consommation Alimentaire - seuils 21/35".
@@ -19,11 +19,26 @@ VALUE LABELS FCSCat21 FCSCat28
 2 "Limite"
 3 "Acceptable".
 
-** calculate Score de diversité alimentaire des ménages
+** calculate Score de diversitÃ© alimentaire des mÃ©nages
 
-compute HDDS = sum(HDDSStapCer,HDDSStapRoot,HDDSPulse,HDDSDairy,HDDSPrMeatF,HDDSPrMeatO,HDDSPrFish,
-HDDSPrEgg,HDDSVegOrg,HDDSVegGre,HDDSVegOth,HDDSFruitOrg,HDDSFruitOth,HDDSFat,HDDSSugar,HDDSCond).
-variable labels HDDS "Score de diversité alimentaire des ménages".
+*combine Meat questions 
+
+compute HDDSPrMeat = sum(HDDSPrMeatF,HDDSPrMeatO).
+recode HDDSPrMeat (0=0) (1 thru highest = 1).
+
+*combine Vegetable questions 
+
+compute HDDSVeg = sum(HDDSVegOrg,HDDSVegGre,HDDSVegOth).
+recode HDDSVeg (0=0) (1 thru highest = 1).
+
+*combine Fruit questions 
+
+compute HDDSFruit = sum(HDDSFruitOrg,HDDSFruitOth).
+recode HDDSFruit (0=0) (1 thru highest = 1).
+
+compute HDDS = sum(HDDSStapCer,HDDSStapRoot,HDDSPulse,HDDSDairy,HDDSPrMeat,HDDSPrFish,
+HDDSPrEgg,HDDSVeg,HDDSFruit,HDDSFat,HDDSSugar,HDDSCond).
+variable labels HDDS "Score de diversitÃ© alimentaire des mÃ©nages".
 
 
 
--- syntax/RBDstandardized_Rsyntaxcombined_FR.R
+++ syntax/RBDstandardized_Rsyntaxcombined_FR.R
@@ -14,32 +14,36 @@ dataset <- to_factor(dataset)
 ##Calculate HHS
 #Recode HHS questions into new variables with score
 dataset  <- dataset  %>% mutate(HHhSNoFood_FR_r = case_when(
-  HHhSNoFood_FR == "Rarely (1–2 times)" ~ 1,
-  HHhSNoFood_FR == "Sometimes (3–10 times)" ~ 1,
-  HHhSNoFood_FR == "Often (more than 10 times)" ~ 2,
+  HHhSNoFood_FR == "Rarement (1 à 2 fois)" ~ 1,
+  HHhSNoFood_FR == "Parfois (3 à 10 fois)" ~ 1,
+  HHhSNoFood_FR == "Souvent (plus que 10 fois)" ~ 2,
   TRUE ~ 0),
   HHhSBedHung_FR_r = case_when(
-    HHhSBedHung_FR == "Rarely (1–2 times)" ~ 1,
-    HHhSBedHung_FR == "Sometimes (3–10 times)" ~ 1,
-    HHhSBedHung_FR == "Often (more than 10 times)" ~ 2,
+    HHhSBedHung_FR == "Rarement (1 à 2 fois)" ~ 1,
+    HHhSBedHung_FR == "Parfois (3 à 10 fois)" ~ 1,
+    HHhSBedHung_FR == "Souvent (plus que 10 fois)" ~ 2,
     TRUE ~ 0),
   HHhSNotEat_FR_r = case_when(
-    HHhSNotEat_FR == "Rarely (1–2 times)" ~ 1,
-    HHhSNotEat_FR == "Sometimes (3–10 times)" ~ 1,
-    HHhSNotEat_FR == "Often (more than 10 times)" ~ 2,
+    HHhSNotEat_FR == "Rarement (1 à 2 fois)" ~ 1,
+    HHhSNotEat_FR == "Parfois (3 à 10 fois)" ~ 1,
+    HHhSNotEat_FR == "Souvent (plus que 10 fois)" ~ 2,
     TRUE ~ 0))
-# Calculate HHhS score
+# Calculate HHS score
 dataset  <- dataset  %>% mutate(HHS = HHhSNoFood_FR_r + HHhSBedHung_FR_r + HHhSNotEat_FR_r)
-var_label(dataset$HHS) <- "Household Hunger Scale"
+var_label(dataHHSEng$HHS) <- "Indice domestique de la faim"
+
 #each household should have an HHS score between 0 - 6
 summary(dataset$HHS)
+
 # Create Categorical HHS
 dataset  <- dataset  %>% mutate(HHSCat = case_when(
-  HHS %in% c(0,1) ~ "No or little hunger in the household",
-  HHS %in% c(2,3) ~ "Moderate hunger in the household",
-  HHS >= 4 ~ "Severe hunger in the household"
+  HHS %in% c(0,1) ~ "Peu ou pas de faim dans le ménage",
+  HHS %in% c(2,3) ~ "Faim modérée dans le ménage",
+  HHS >= 4 ~ "Faim sévère dans le ménage"
 ))
-var_label(dataset$HHSCat) <- "Household Hunger Score Categories"
+var_label(dataset$HHSCat) <- "Catégories de la faim dans les ménages"
+
+
 
 ##calculate rCSI
 dataset <- dataset %>% mutate(rCSI = rCSILessQlty  + (2 * rCSIBorrow) + rCSIMealSize + (3 * rCSIMealAdult) + rCSIMealNb)
@@ -47,64 +51,80 @@ var_label(dataset$rCSI) <- "rCSI"
 
 #calculate FCS
 dataset <- dataset %>% mutate(FCS = (2 * FCSStap) + (3 * FCSPulse)+ (4*FCSPr) +FCSVeg  +FCSFruit +(4*FCSDairy) + (0.5*FCSFat) + (0.5*FCSSugar))
-#create FCG groups based on 21/25 or 28/42 thresholds
+var_label(dataset$FCS) <- "Score de consommation alimentaire"
+#create FCG groups based on 21/25 or 28/42 thresholds - analyst should decide which one to use.
 dataset <- dataset %>% mutate(
   FCSCat21 = case_when(
-    FCS <= 21 ~ "Poor", between(FCS, 21.5, 35) ~ "Borderline", FCS > 35 ~ "Acceptable"),
+    FCS <= 21 ~ "Pauvre", between(FCS, 21.5, 35) ~ "Limite", FCS > 35 ~ "Acceptable"),
   FCSCat28 = case_when(
-    FCS <= 28 ~ "Poor", between(FCS, 28.5, 42) ~ "Borderline", FCS > 42 ~ "Acceptable"))
-var_label(dataset$FCSCat21) <- "Food Consumption Group 21/35 thresholds"
-var_label(dataset$FCSCat28) <-  "Food Consumption Group 28/42 thresholds"
+    FCS <= 28 ~ "Pauvre", between(FCS, 28.5, 42) ~ "Limite", FCS > 42 ~ "Acceptable"))
+var_label(dataset$FCSCat21) <- "Groupe de consommation alimentaire - Seuils du 21/35"
+var_label(dataset$FCSCat28) <-  "Groupe de consommation alimentaire - Seuils du 28/42"
 
-#calculate HDDS
-#first by creating the 12 groups based on the 16 questions
+
+
+#calculate HDDS first by creating the 12 groups based on the 16 questions
 dataset <- dataset %>% mutate(
-  HDDSStapCer = case_when(HDDSStapCer == "Yes" ~ 1, TRUE ~ 0),
-  HDDSStapRoot = case_when(HDDSStapRoot  == "Yes" ~ 1, TRUE ~ 0),
-  HDDSVeg = case_when(HDDSVegOrg  == "Yes" | HDDSVegGre == "Yes" | HDDSVegOth == "Yes" ~ 1, TRUE ~ 0),
-  HDDSFruit = case_when(HDDSFruitOrg == "Yes" | HDDSFruitOth == "Yes" ~ 1, TRUE ~ 0),
-  HDDSPrMeat = case_when(HDDSPrMeatF == "Yes" | HDDSPrMeatO == "Yes" ~ 1, TRUE ~ 0),
-  HDDSPrEgg = case_when(HDDSPrEgg  == "Yes" ~ 1, TRUE ~ 0),
-  HDDSPrFish = case_when(HDDSPrFish == "Yes" ~ 1, TRUE ~ 0),
-  HDDSPulse = case_when(HDDSPulse == "Yes" ~ 1, TRUE ~ 0),
-  HDDSDairy = case_when(HDDSDairy == "Yes" ~ 1, TRUE ~ 0),
-  HDDSFat = case_when(HDDSFat == "Yes" ~ 1, TRUE ~ 0),
-  HDDSSugar = case_when(HDDSSugar == "Yes" ~ 1, TRUE ~ 0),
-  HDDSCond = case_when(HDDSCond == "Yes"~ 1, TRUE ~ 0))
+  HDDSStapCer = case_when(HDDSStapCer == "Oui" ~ 1, TRUE ~ 0),
+  HDDSStapRoot = case_when(HDDSStapRoot  == "Oui" ~ 1, TRUE ~ 0),
+  HDDSVeg = case_when(HDDSVegOrg  == "Oui" | HDDSVegGre == "Oui" | HDDSVegOth == "Oui" ~ 1, TRUE ~ 0),
+  HDDSFruit = case_when(HDDSFruitOrg == "Oui" | HDDSFruitOth == "Oui" ~ 1, TRUE ~ 0),
+  HDDSPrMeat = case_when(HDDSPrMeatF == "Oui" | HDDSPrMeatO == "Oui" ~ 1, TRUE ~ 0),
+  HDDSPrEgg = case_when(HDDSPrEgg  == "Oui" ~ 1, TRUE ~ 0),
+  HDDSPrFish = case_when(HDDSPrFish == "Oui" ~ 1, TRUE ~ 0),
+  HDDSPulse = case_when(HDDSPulse == "Oui" ~ 1, TRUE ~ 0),
+  HDDSDairy = case_when(HDDSDairy == "Oui" ~ 1, TRUE ~ 0),
+  HDDSFat = case_when(HDDSFat == "Oui" ~ 1, TRUE ~ 0),
+  HDDSSugar = case_when(HDDSSugar == "Oui" ~ 1, TRUE ~ 0),
+  HDDSCond = case_when(HDDSCond == "Oui"~ 1, TRUE ~ 0))
 
 #Calculate HDDS
 dataset <- dataset %>% mutate(HDDS = HDDSStapCer +HDDSStapRoot +HDDSVeg +HDDSFruit +HDDSPrMeat +HDDSPrEgg +HDDSPrFish +HDDSPulse +HDDSDairy +HDDSFat +HDDSSugar +HDDSCond)
-var_label(dataset$HDDS) <- "Hosehold Dietary Diversity Score"
+var_label(dataset$HDDS) <- "Score de la diversité alimentaire des ménages"
+
+
+
+
+
+
+
+
+
 
 #Calculate LHCS
-#stress
+#parce que le texte est très long dans les questions - voir si la réponse a été tronquée
+levels(dataset$LhCSIStress1)
+
 dataset <- dataset %>% mutate(stress_coping = case_when(
-  LhCSIStress1 %in% c("Yes","No; because I already sold those assets or did this activity in the last 12 months and cannot continue to do it") ~ "Yes",
-  LhCSIStress2 %in% c("Yes","No; because I already sold those assets or did this activity in the last 12 months and cannot continue to do it") ~ "Yes",
-  LhCSIStress3 %in% c("Yes","No; because I already sold those assets or did this activity in the last 12 months and cannot continue to do it") ~ "Yes",
-  LhCSIStress4 %in% c("Yes","No; because I already sold those assets or did this activity in the last 12 months and cannot continue to do it") ~ "Yes",
-  TRUE ~ "No"))
-var_label(dataset$stress_coping) <- "Did the HH engage in stress coping strategies"
+  LhCSIStress1 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas c") ~ "Oui",
+  LhCSIStress2 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas c") ~ "Oui",
+  LhCSIStress3 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas c") ~ "Oui",
+  LhCSIStress4 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas c") ~ "Oui",
+  TRUE ~ "Non"))
+var_label(dataset$stress_coping) <- "le ménage s'est-il engagé dans des stratégies  du stress ?"
+
 #Crisis
 dataset <- dataset %>% mutate(crisis_coping = case_when(
-  LhCSICrisis1 %in% c("Yes","No; because I already sold those assets or did this activity in the last 12 months and cannot continue to do it") ~ "Yes",
-  LhCSICrisis2 %in% c("Yes","No; because I already sold those assets or did this activity in the last 12 months and cannot continue to do it") ~ "Yes",
-  LhCSICrisis3 %in% c("Yes","No; because I already sold those assets or did this activity in the last 12 months and cannot continue to do it") ~ "Yes",
-  TRUE ~ "No"))
-var_label(dataset$crisis_coping) <- "Did the HH engage in crisis coping strategies"
+  LhCSICrisis1 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas c") ~ "Oui",
+  LhCSICrisis2 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas c") ~ "Oui",
+  LhCSICrisis3 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas c") ~ "Oui",
+  TRUE ~ "Non"))
+var_label(dataset$crisis_coping) <- "le ménage s'est-il engagé dans des stratégies d'adaptation aux crises ?"
+
 #Emergency
 dataset <- dataset %>% mutate(emergency_coping = case_when(
-  LhCSIEmergency1 %in% c("Yes","No; because I already sold those assets or did this activity in the last 12 months and cannot continue to do it") ~ "Yes",
-  LhCSIEmergency2 %in% c("Yes","No; because I already sold those assets or did this activity in the last 12 months and cannot continue to do it") ~ "Yes",
-  LhCSIEmergency3 %in% c("Yes","No; because I already sold those assets or did this activity in the last 12 months and cannot continue to do it") ~ "Yes",
-  TRUE ~ "No"))
-var_label(dataset$emergency_coping) <- "Did the HH engage in emergency coping strategies"
+  LhCSIEmergency1 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas c") ~ "Oui",
+  LhCSIEmergency2 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas c") ~ "Oui",
+  LhCSIEmergency3 %in% c("Oui","Non, parce que j’ai déjà vendu ces actifs ou mené cette activité au cours des 12 derniers mois et je ne peux pas c") ~ "Oui",
+  TRUE ~ "Non"))
+var_label(dataset$emergency_coping) <- "le ménage s'est-il engagé dans des stratégies d'adaptation d'urgence ?"
+
+
 #calculate Max_coping_behaviour
 dataset <- dataset %>% mutate(LhCSICat = case_when(
-  emergency_coping == "Yes" ~ "EmergencyStrategies",
-  crisis_coping == "Yes" ~ "CrisisStrategies",
-  stress_coping == "Yes" ~ "StressStrategies",
-  TRUE ~ "NoStrategies"))
-var_label(dataset$LhCSICat) <- "Livelihood Coping Strategy categories - CARI light version"
+  emergency_coping == "Oui" ~ "StrategiesdeUrgence",
+  crisis_coping == "Oui" ~ "StrategiesdeCrise",
+  stress_coping == "Oui" ~ "StrategiesdeStress",
+  TRUE ~ "Pasdestrategies"))
 
-
+var_label(dataset$LhCSICat) <- "Catégories de stratégies d'adaptation aux moyens d'existence - version léger  de CARI"
